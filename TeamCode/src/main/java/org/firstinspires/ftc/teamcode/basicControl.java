56505f460b4f783a5fa087fb6d3da83ddd0370e5
nzunder136 <noah.zunder@icloud.com>
2023-10-08 15:51:07 -0400

    noah branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicControl.java
@@ -1,20 +1,14 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-
 @Autonomous(name="YourAuto", group="Autonomous")
 //@Disabled
-
     /* Initialize standard Hardware interfaces */
 public class basicControl extends LinearOpMode { //* Change name to your liking
-
     //* Variable Declaration
     MecanumHMap robot = new MecanumHMap(); //hardware map object
     ElapsedTime runtime = new ElapsedTime();
-
     //Main method, runs after init button pressed on DS
     @Override
     public void runOpMode() {
@@ -36,8 +30,6 @@ public class basicControl extends LinearOpMode { //* Change name to your liking
         //moveBackwards();
         //*Main code, runs after start button pressed
     }
-
-
     //* Waits for "B" button to be pressed
     //Hint: "game pad1.b" returns true if "B" pressed, false if not
     public void waitForB_Button(){
@@ -47,7 +39,6 @@ public class basicControl extends LinearOpMode { //* Change name to your liking
             }
         }
     }
-
     //* Robot goes forward 5 seconds
     public void runForward(){
         while(gamepad1.dpad_up){
@@ -60,21 +51,18 @@ public class basicControl extends LinearOpMode { //* Change name to your liking
         robot.RTMotor.setPower(0);
         robot.LBMotor.setPower(0);
         robot.RBMotor.setPower(0);
-
-
     }
     public void turnLeft(){
         while(gamepad1.dpad_left){
             robot.LTMotor.setPower(.1);
             robot.RTMotor.setPower(.1);
             robot.LBMotor.setPower(.1);
-            robot.RBMotor.setPower(.1);
+            robot.RBMotor.setPower(-.1);
         }
         robot.LTMotor.setPower(0);
         robot.RTMotor.setPower(0);
         robot.LBMotor.setPower(0);
         robot.RBMotor.setPower(0);
-
     }
     public void turnRight(){
         while(gamepad1.dpad_right){
@@ -87,21 +75,6 @@ public class basicControl extends LinearOpMode { //* Change name to your liking
         robot.RTMotor.setPower(0);
         robot.LBMotor.setPower(0);
         robot.RBMotor.setPower(0);
-
-    }
-    public void turnLeftAndMove(){
-        turnLeft();
-        runForward();
-        telemetry.addData("turnLeft and move", runtime.seconds());
-        telemetry.update();
-
-    }
-    public void turnRightAndMove(){
-        turnRight();
-        runForward();
-        telemetry.addData("moving-right", runtime.seconds());
-        telemetry.update();
-
     }
     public void moveBackwards() {
         while (gamepad1.dpad_down) {
@@ -116,10 +89,3 @@ public class basicControl extends LinearOpMode { //* Change name to your liking
         robot.RBMotor.setPower(0);
     }
 }
-
-
-
-
-
-
-
