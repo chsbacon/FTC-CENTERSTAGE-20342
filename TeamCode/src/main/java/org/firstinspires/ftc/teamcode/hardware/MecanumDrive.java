12751c9b2e845a1ef3e6b9394e130a5f12bfe1eb
Pweder69 <pweder07@charlottesvilleschools.org>
2023-10-12 21:16:04 -0400

    tuning copied from 7080

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MecanumDrive.java
@@ -5,7 +5,9 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.*;
+import com.acmerobotics.roadrunner.AccelConstraint;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
 import com.acmerobotics.roadrunner.AngularVelConstraint;
 import com.acmerobotics.roadrunner.DualNum;
 import com.acmerobotics.roadrunner.HolonomicController;
@@ -14,7 +16,10 @@ import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.MotorFeedforward;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.PoseVelocity2dDual;
 import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Time;
 import com.acmerobotics.roadrunner.TimeTrajectory;
 import com.acmerobotics.roadrunner.TimeTurn;
@@ -38,9 +43,7 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.hardware.Localizer;
 
-import java.lang.Math;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -49,14 +52,15 @@ import java.util.List;
 public final class MecanumDrive {
     public static class Params {
         // drive model parameters
-        public double inPerTick = 2023/47;
-        public double lateralInPerTick = 1628/36;
-        public double trackWidthTicks = 916.6418935229276;
+        // drive model parameters
+        public double inPerTick = 0.023564;
+        public double lateralInPerTick = 0.023762;
+        public double trackWidthTicks = 873;
 
         // feedforward parameters (in tick units)
-        public double kS = 0.7006843798364581;
-        public double kV = 0.0042965152604539135;
-        public double kA = 0;
+        public double kS = 0.960801;
+        public double kV = 0.00424555;
+        public double kA = 0.000009;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -68,13 +72,14 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 0.5;
+        public double lateralGain = 1.0;
+        public double headingGain = 5.0; // shared with turn
 
-        public double axialVelGain = 0.0;
+        public double axialVelGain = 1.0;
         public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double headingVelGain = 0.5; // shared with turn
+
     }
 
     public static Params PARAMS = new Params();
