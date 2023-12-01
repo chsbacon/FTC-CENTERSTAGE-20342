2a22dff31b3cc3f9aefa6980418ce9719835ac99
Pweder69 <pweder07@charlottesvilleschools.org>
2023-11-30 20:32:12 -0500

    Basic structure for Arm Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/HMap.java
@@ -4,19 +4,25 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.openftc.easyopencv.OpenCvCamera;
 
 
 public class HMap
 {
-    public DcMotor  LFMotor    = null;
-    public DcMotor  RFMotor     = null;
-    public DcMotor  LBMotor    = null;
-    public DcMotor  RBMotor      = null;
-
+    public DcMotor  LFMotor = null;
+    public DcMotor  RFMotor = null;
+    public DcMotor  LBMotor = null;
+    public DcMotor  RBMotor = null;
+    public DcMotor  armMotor = null;
+    public Servo clawServo = null;
+    public Servo launcherServo = null;
     public IMU imu = null;
+    public OpenCvCamera webcam = null;
 
-    HardwareMap hwMap           =  null;
+    HardwareMap hwMap = null;
     private ElapsedTime period  = new ElapsedTime();
 
     /* Constructor */
@@ -29,11 +35,13 @@ public class HMap
         // Save reference to Hardware map
         hwMap = ahwMap;
         // Define and Initialize Motors
-        LFMotor    = hwMap.get(DcMotor.class, "leftFront");
-        RFMotor    = hwMap.get(DcMotor.class, "rightFront");
-        LBMotor    = hwMap.get(DcMotor.class, "leftBack");
-        RBMotor    = hwMap.get(DcMotor.class, "rightBack");
-
+        LFMotor = hwMap.get(DcMotor.class, "leftFront");
+        RFMotor = hwMap.get(DcMotor.class, "rightFront");
+        LBMotor = hwMap.get(DcMotor.class, "leftBack");
+        RBMotor = hwMap.get(DcMotor.class, "rightBack");
+        armMotor = hwMap.get(DcMotor.class, "arm");
+        clawServo = hwMap.get(Servo.class, "claw");
+        launcherServo = hwMap.get(Servo.class, "launcher");
         imu = hwMap.get(IMU.class, "imu");
 
         IMU.Parameters myIMUparameters;
@@ -41,12 +49,11 @@ public class HMap
         myIMUparameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
                         RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
-                        RevHubOrientationOnRobot.UsbFacingDirection.UP // depends on install
-                        // https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html
-                        // Link to direction guide.
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
                 )
         );
 
+        imu.initialize(myIMUparameters);
 
         RBMotor.setDirection(DcMotor.Direction.REVERSE);
         LFMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -57,6 +64,7 @@ public class HMap
         RFMotor.setPower(0);
         LBMotor.setPower(0);
         RBMotor.setPower(0);
+        armMotor.setPower(0);
 
         //stop and reset?
         // Set motors to run with encoders.
@@ -64,11 +72,13 @@ public class HMap
         RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Set zero power behavior
         LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 }
