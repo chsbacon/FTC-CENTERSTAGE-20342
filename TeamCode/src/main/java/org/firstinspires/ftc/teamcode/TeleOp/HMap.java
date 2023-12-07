361d63306a565c8990353e19b314a4bec3474630
honklin <hazeljconklin@gmail.com>
2023-12-07 16:41:15 -0500

    TeleOp and Vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HMap.java
@@ -4,19 +4,25 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.openftc.easyopencv.OpenCvCamera;
 
 
 public class HMap
 {
-    public DcMotor  LFMotor    = null;
-    public DcMotor  RFMotor     = null;
-    public DcMotor  LBMotor    = null;
-    public DcMotor  RBMotor      = null;
-
+    public DcMotor  LFMotor = null;
+    public DcMotor  RFMotor = null;
+    public DcMotor  LBMotor = null;
+    public DcMotor  RBMotor = null;
+    public DcMotor  armMotor = null;
+    public Servo clawServo = null;
+    public Servo launcherServo = null;
     public IMU imu = null;
+    public OpenCvCamera Webcam1 = null;
 
-    HardwareMap hwMap           =  null;
+    HardwareMap hwMap = null;
     private ElapsedTime period  = new ElapsedTime();
 
     /* Constructor */
@@ -26,22 +32,24 @@ public class HMap
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
-        // Save reference to Hardware map
+        //Save reference to hardware map
         hwMap = ahwMap;
         // Define and Initialize Motors
-        LFMotor    = hwMap.get(DcMotor.class, "leftFront");
-        RFMotor    = hwMap.get(DcMotor.class, "rightFront");
-        LBMotor    = hwMap.get(DcMotor.class, "leftBack");
-        RBMotor    = hwMap.get(DcMotor.class, "rightBack");
-
+        LFMotor = hwMap.get(DcMotor.class, "leftFront");
+        RFMotor = hwMap.get(DcMotor.class, "rightFront");
+        LBMotor = hwMap.get(DcMotor.class, "leftBack");
+        RBMotor = hwMap.get(DcMotor.class, "rightBack");
+        armMotor = hwMap.get(DcMotor.class, "arm");
+        clawServo = hwMap.get(Servo.class, "claw");
+        launcherServo = hwMap.get(Servo.class, "launcher");
         imu = hwMap.get(IMU.class, "imu");
 
         IMU.Parameters myIMUparameters;
 
         myIMUparameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD // depends on install
+                        RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP // depends on install
                         // https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html
                         // Link to direction guide.
                 )
@@ -57,6 +65,7 @@ public class HMap
         RFMotor.setPower(0);
         LBMotor.setPower(0);
         RBMotor.setPower(0);
+        armMotor.setPower(0);
 
         //stop and reset?
         // Set motors to run with encoders.
@@ -64,11 +73,13 @@ public class HMap
         RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Set zero power behavior
         LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp20342.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp20342.java
@@ -2,17 +2,11 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@TeleOp(name = "TestTeleOp", group = "TeleOp")
-public class TestTeleOp extends LinearOpMode {
+@TeleOp(name = "TeleOp20342", group = "TeleOp")
+public class TeleOp20342 extends LinearOpMode {
     HMap robot = new HMap(); //hardware map object
     ElapsedTime runtime = new ElapsedTime();
 
@@ -38,12 +32,45 @@ public class TestTeleOp extends LinearOpMode {
                     Math.max(Math.abs(RFpwr), Math.abs(RBpwr)));
             if (Math.abs(turnScale) < 1.0) turnScale = 1.0;
 
+            double armPwr = 0;
+            if (gamepad1.right_trigger > 0.75) {
+                  armPwr = .5;
+            }
+            else if (gamepad1.left_trigger > 0.75) {
+                armPwr = -.5;
+            }
+            else {
+                armPwr = 0;
+            }
+
+            double launcherPwr = 1;
+            if (gamepad1.x) {
+                launcherPwr = 1;
+                telemetry.addData("Launcher: ", robot.launcherServo.getPosition());
+            }
+            else {
+                launcherPwr = 0;
+                telemetry.addData("Launcher: ", robot.launcherServo.getPosition());
+            }
+
+            double clawPwr = 0;
+            if (gamepad1.y) {
+                clawPwr = 1;
+                telemetry.addData("Claw: ", clawPwr);
+            }
+            else {
+                clawPwr = 0;
+            }
+            telemetry.update();
+
             // set the motors
             robot.LFMotor.setPower(LFpwr / turnScale);
             robot.RBMotor.setPower(LBpwr / turnScale);
             robot.RFMotor.setPower(RFpwr / turnScale);
             robot.LBMotor.setPower(RBpwr / turnScale);
-
+            robot.armMotor.setPower(armPwr);
+            robot.launcherServo.setPosition(launcherPwr);
+            robot.clawServo.setPosition(clawPwr);
         }
     }
 
